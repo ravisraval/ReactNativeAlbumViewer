d485f7107d977a5c7974c27862637d09dd7e339f
Ravi <raviraval@gmail.com>
2018-02-08 12:41:46 -0800

    add background task capability

--- android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new BackgroundTaskPackage()
       );
     }
 
@@ -41,5 +43,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    BackgroundTaskPackage.useContext(this);
   }
 }
